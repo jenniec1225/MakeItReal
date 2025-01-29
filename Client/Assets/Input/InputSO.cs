05eaab832e7bd1345827eac0d681c94958241ff0
jenniec1225 <142662931+jenniec1225@users.noreply.github.com>
2025-01-29 23:41:39 +0900

    캐릭터 움직임 구현

---
+++ Client/Assets/Input/InputSO.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/InputSO", fileName = "InputSO")]
+public class InputSO : ScriptableObject
+{
+    public InputSystem InputSystem;
+
+    public UnityAction<Vector2> moveAction;
+    public UnityAction<Vector2> lookAction;
+
+    private void OnEnable()
+    {
+        InputSystem = new InputSystem();
+        InputSystem.InGame.Enable();
+
+        InputSystem.InGame.Move.performed += ctx => moveAction.Invoke(ctx.ReadValue<Vector2>());
+
+        InputSystem.InGame.Move.canceled += ctx => moveAction?.Invoke(Vector2.zero);
+
+        //입력마우스
+
+        InputSystem.InGame.Look.performed += ctx => lookAction.Invoke(ctx.ReadValue<Vector2>());
+        InputSystem.InGame.Look.canceled += ctx => lookAction.Invoke(Vector2. zero);
+
+    }
+
+    private void OnDisable()
+    {
+        InputSystem.InGame.Move.performed -= ctx => moveAction.Invoke(ctx.ReadValue<Vector2>());
+
+        InputSystem.InGame.Move.canceled -= ctx => moveAction?.Invoke(Vector2.zero);
+
+        InputSystem.InGame.Look.performed -= ctx => lookAction.Invoke(ctx.ReadValue<Vector2>());
+
+        InputSystem.InGame.Disable();
+    }
+
+}
---
+++ Client/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private InputSO inputSO;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 2f;
+
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private CharacterController characterController;
+
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+
+
+    }
+
+    private void OnEnable()
+    {
+        inputSO.moveAction += OnMoveInput;
+        inputSO.lookAction += OnLookInput;
+    }
+
+    private void OnDisable()
+    {
+        inputSO.moveAction -= OnMoveInput;
+        inputSO.lookAction -= OnLookInput;
+
+    }
+
+    private void OnMoveInput(Vector2 input)
+    {
+        moveInput = input;
+    }
+
+    private void OnLookInput(Vector2 input)
+    {
+        lookInput = input;
+    }
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        characterController.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(Vector3.up * lookInput.x * rotationSpeed);
+    }
+}
